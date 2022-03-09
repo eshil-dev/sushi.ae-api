f6781359489425ed247d03d8a28e82bc9271c062
Hashem Poya <poya.kpu.2020@gmail.com>
2022-03-09 17:38:07 +0430

    [ADD] User shchema and model created for users.

---
+++ srs/models/schemas/user.schema.js
@@ -0,0 +1,35 @@
+import mongoose from 'mongoose';
+import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
+
+import { config } from '../../config/db.config';
+
+const userSchema = new mongoose.Schema({
+    email: {
+        type: String,
+        required: true,
+        trim: true,
+        unique: true
+      },
+    password: {
+        type: String,
+        required: true
+    },
+    phone: {
+        type: String,
+        required: true,
+    },
+});
+
+userSchema.methods.generateAuthToken = function() {
+    const token = jwt.sign({ _id: this._id, name: this.name, email: this.email }, config.passport.secret, {
+        expiresIn: 1000000,
+    });
+    return token;
+}
+
+userSchema.methods.comparePassword = function comparePassword(password) {
+    return bcrypt.compareSync(password, this.password);
+}
+
+export default userSchema;
