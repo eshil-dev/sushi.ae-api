6a414c10942682b5d9790b6e8ed6f44841d21a23
Hashem Poya <poya.kpu.2020@gmail.com>
2022-03-15 10:49:23 +0430

    [ADD] Created separt endpoint for uploading image plus update, delete menu and category.

--- srs/controllers/menu.controller.js
+++ srs/controllers/menu.controller.js
@@ -1,22 +1,79 @@
+import fs from 'fs';
+import path from 'path';
 import Category from '../models/menu/category.model';
 import Menu from '../models/menu/menu.model';
+import uploadFileMiddleware from '../middleware/image-upload';
+
+const __dirname = path.resolve();
+
+export const uploadPhoto = async (req, res) => {
+  try {
+    await uploadFileMiddleware(req, res);
+    if (req.file == undefined) {
+      return res.status(400).send({ message: "Please upload a file!" });
+    }
+    res.status(200).send({
+      message: "Uploaded the file successfully: ",
+    });
+  } catch (err) { 
+    res.status(500).send({
+      message: `Could not upload the file: . ${err}`,
+    });
+  }
+
+}
 
 export const listCategory = async (req, res) => {
+  try {
     const result = await Category.find({});
-    res.send(result);
+    return res.send(result);
+  }catch(err) {
+    return res.send(err);
+  }
+ 
 }
 
 export const postCategory = async (req, res) => {
-    const { name } = req.body;
-    const category = Category({
-        name: name
-    });
+  try {
+    const category = Category({name: req.body.name});
     const result = await category.save();
-    res.send({message: result});
+    return res.send({message: result});
+  }catch(err) {
+    return res.send(err);
+  }
+}
+
+export const updateCategory = async (req, res) => {
+  try {
+    let category = await Category.findById(req.params.id);
+    if(!category) {
+      return res.send({message: 'Category not found!'});
+    }
+    await category.update(req.body);
+    res.send({id: category._id,  ...req.body})
+  }catch(err) {
+    res.send({message: err.message});
+  }
+}
+
+export const deleteCategory = async (req, res) => {
+  const catId = req.params.id;
+  try {
+    let category = await Category.findById(catId);
+    if(!category) {
+      res.send({message: 'Category not found to delete'});
+    }
+    await Category.findByIdAndDelete(catId)
+    res.send({message: 'Category Deleted successfully.'});
+
+  }catch(err) {
+    res.send({message: err.message});
+  }
 }
 
 export const postMenu =  async (req, res) => {
-    const { name, description, imageUrl, price, category } = req.body;
+  try{
+    const {name, description, imageUrl, price, category} = req.body;
     const menu = Menu({
         name: name,
         description: description,
@@ -25,10 +82,56 @@ export const postMenu =  async (req, res) => {
         category: category
     });
     const result = await menu.save();
-    res.send(result);
+    return res.send(result);
+  }catch(err) {
+    return res.send(err);
+  }
+  
 };
 
 export const listMenu = async (req, res) => {
-    const result = await Menu.find().populate('category');
-    res.send(result);
+    const directoryPath = __dirname + "/uploads/";
+    fs.readdir(directoryPath, async function (err, files) {
+      if (err) {
+        res.status(500).send({
+          message: "Unable to scan files!",
+        });
+      }
+      let fileInfos = [];
+      files.forEach((file) => {
+        fileInfos.push({
+          name: file,
+          url: __dirname + '/uploads/' + file,
+        });
+      });
+      const result = await Menu.find().populate('category');
+      return res.status(200).send({data: result, file: fileInfos});
+    });
+}
+
+export const updateMenu = async (req, res) => {
+  try {
+    const menu = await Menu.findById(req.params.id);
+    if(!menu) {
+      return res.send({message: 'Menu with the provided ID is not found'});
+    }
+    await menu.update(req.body);
+    return res.send({id: menu._id,  ...req.body})
+  }catch(err) {
+    return res.send(err);
+  }
+}
+
+export const deleteMenu = async (req, res) => {
+  try {
+    const menuId = req.params.id;
+    const menu = await Menu.findById(menuId);
+    if(!menu) {
+      return res.send({message: 'Menu with the provided ID is not found.'});
+    }
+    await Menu.findByIdAndDelete(menuId);
+    return res.send({message: 'Menu is succesfully deleted.'});
+  }catch(err) {
+    return res.send(err)
+  }
 }
--- srs/routes/menu.route.js
+++ srs/routes/menu.route.js
@@ -1,18 +1,39 @@
 import express from 'express';
 import passport from 'passport';
 
-import { listCategory, postCategory, listMenu, postMenu } from '../controllers/menu.controller';
+import {
+        listCategory,
+        postCategory,
+        updateCategory,
+        deleteCategory,
+        uploadPhoto,
+        listMenu,
+        postMenu,
+        updateMenu,
+        deleteMenu
+} from '../controllers/menu.controller';
 
 const menuRouter = express.Router();
 
 menuRouter.use(passport.authenticate('jwt', { session: false }));
 
-menuRouter.route('/')
-        .post(postMenu)
-        .get(listMenu);
+menuRouter.route('/upload')
+        .post(uploadPhoto);
 
 menuRouter.route('/category')
         .post(postCategory)
         .get(listCategory);
+
+menuRouter.route('/category/:id')
+        .put(updateCategory)
+        .delete(deleteCategory)
+
+menuRouter.route('/')
+        .post(postMenu)
+        .get(listMenu);
+
+menuRouter.route('/:id')
+        .put(updateMenu)
+        .delete(deleteMenu)
 
 export default menuRouter;
