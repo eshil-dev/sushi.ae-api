5680d2b8dc169d7f8e758447c3e604e17fc14d25
Hashem Poya <poya.kpu.2020@gmail.com>
2022-07-01 22:29:13 +0430

    [ADD] Added route for updating order status.

--- src/routes/order.route.js
+++ src/routes/order.route.js
@@ -1,10 +1,11 @@
 import express from "express";
 
-import { listOrder, postOrder } from "../controllers/order.controller";
+import { listOrder, postOrder, updateOrderStatus } from "../controllers/order.controller";
 
 const orderRouter = express.Router();
 
 orderRouter.route('/').get(listOrder);
 orderRouter.route('/').post(postOrder);
+orderRouter.route('/:id').patch(updateOrderStatus);
 
 export default orderRouter;
