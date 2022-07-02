f0bde02c6adedac064562e57f6e3a8a48bc2afea
Zaman Ehsani <zamanehsani@gmail.com>
2022-07-02 20:38:17 +0400

    added the node inside start instead of nodemon in package.json and added the extension of .js in order.route import

--- src/routes/order.route.js
+++ src/routes/order.route.js
@@ -1,6 +1,6 @@
 import express from "express";
 
-import { listOrder, postOrder, updateOrderStatus } from "../controllers/order.controller";
+import { listOrder, postOrder, updateOrderStatus } from "../controllers/order.controller.js";
 
 const orderRouter = express.Router();
 
