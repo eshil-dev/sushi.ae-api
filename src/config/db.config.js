7f8e0574f7292fc7fc6960aa189500b3f553cde9
Hashem Poya <poya.kpu.2020@gmail.com>
2022-07-16 13:37:07 +0430

    Removed unnecessary import.

--- src/config/db.config.js
+++ src/config/db.config.js
@@ -1,20 +1,16 @@
-import 'dotenv/config';
-
 export const config = {
-    passport: {
-      secret: 'node.js_sample_secret_key_123',
-      expiresIn: 10000,
-    },
-    env: {
-      port: process.env.PORT || 8080 /*the port of defualt in case port is not inside env file*/,
-      mongoDBUri:
-        process.env.ENV === 'prod'
-          ? `mongodb+srv://${process.env.DB_USER_NAME}:${process.env.DB_USER_PASSWORD}@cluster0.dlq10.mongodb.net/?retryWrites=true&w=majority`
-          : 'mongodb://localhost/freshmenu_db',
-      mongoHostName: process.env.ENV === 'prod' ? 'mongodbAtlas' : 'localhost',
-    },
-  };
-  console.log('on env:',process.env.ENV)
-  console.log('on env:',process.env.DB_USER_NAME)
+  passport: {
+    secret: 'node.js_sample_secret_key_123',
+    expiresIn: 10000,
+  },
+  env: {
+    port: process.env.PORT || 8080 /*the port of defualt in case port is not inside env file*/,
+    mongoDBUri:
+      process.env.ENV === 'prod'
+        ? `mongodb+srv://${process.env.DB_USER_NAME}:${process.env.DB_USER_PASSWORD}@cluster0.dlq10.mongodb.net/?retryWrites=true&w=majority`
+        : 'mongodb://localhost/freshmenu_db',
+    mongoHostName: process.env.ENV === 'prod' ? 'mongodbAtlas' : 'localhost',
+  },
+};
+
 export const underscoreId = '_id';
-  
